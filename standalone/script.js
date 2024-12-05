036c42c7df8667f0815a51a60177cbd053aaf80d
agg22 <amber.gorzynski22@imperial.ac.uk>
2024-12-05 11:42:06 +0000

    Add absolute path to dawn.node build

--- standalone/script.js
+++ standalone/script.js
@@ -1,4 +1,4 @@
-const { create, globals } = require("./dawn.node");
+const { create, globals } = require("/data/dev/dawn/build/dawn.node");
 Object.assign(globalThis, globals); // Provides constants like GPUBufferUsage.MAP_READ
 let navigator = { gpu: create([]) };
 
