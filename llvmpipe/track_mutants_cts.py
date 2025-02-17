3da716155acc91910d4a3f6c475c08e85cd22829
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-02-17 11:58:03 +0000

    Add script to analyse cts mutant tracking

---
+++ llvmpipe/track_mutants_cts.py
@@ -0,0 +1,93 @@
+import subprocess
+import os
+from pathlib import Path
+
+from mutate_mesa import clean, get_files_for_mutation, mutate
+from run.cts.utils import run_cts
+from run.wgslsmith.utils import run_wgslsmith_program
+
+def track(mesa, dredd, mutation_dir, info_file, compile_commands):
+
+    clean(mesa)
+
+    mutation_files = get_files_for_mutation(compile_commands, 
+                                        mutation_dir, 
+                                        mesa)
+
+    mutate(dredd,
+        mutation_files,
+        info_file,
+        compile_commands,
+        mesa,
+        track_only=True)
+
+    build(mesa)
+    install(mesa)
+
+def track(track_cts : bool = True, track_wgslsmith : bool = True):
+
+    base = Path('/data/dev')
+    dredd = Path(base,'dredd/third_party/clang+llvm/bin/dredd')
+    mesa = Path(base, 'mesa_tracked')
+    mutation_dir = Path('src')
+    info_file = Path(mesa,'mutant_info.json')
+    compile_commands = Path(mesa, 'build', 'compile_commands.json')
+
+    cts = Path('/data/dev/webgpu_cts')
+    dawn = Path('/data/dev/dawn')
+    mesa_vk_icd = Path(mesa, 'build/install/share/vulkan/icd.d/lvp_icd.x86_64.json')
+
+    #track(mesa, dredd, mutation_dir, info_file, compile_commands)
+
+    if track_cts:
+        for i in range(10):
+            tracking_file = Path(f'tracking/tracking_file_run_{i}.txt').resolve()
+            run_cts(cts, dawn, mesa_vk_icd, output_name = f'tracking/tracking_run_{i}', tracking_file = tracking_file)
+
+    if track_wgslsmith:
+        for i in range(100):
+            tracking_file = Path(f'wgslsmith/tracking/tracking_file_wgslsmith_run_{i}.txt').resolve()
+            wgslsmith_program = Path(f'wgslsmith/wgslsmith_prog_{i}.js').resolve()
+            run_wgslsmith_program(wgslsmith_program,
+                mesa_vk_icd,
+                generate=True,
+                tracking=tracking_file)
+
+def process_tracking(cts : Path, wgslsmith : Path):
+
+    if cts is not None:
+        cts_mutants = get_mutants(cts)
+        print(f'The CTS covers {len(cts_mutants)} mutants in total')
+
+    if wgslsmith is not None:
+        wgslsmith_mutants = get_mutants(wgslsmith)
+        print(f'A sample of 100 WGSLsmith tests cover {len(wgslsmith_mutants)} mutants in total')
+
+    if cts is not None and wgslsmith is not None:
+        covered_by_both = cts_mutants.intersection(wgslsmith_mutants)
+        covered_by_cts_only = cts_mutants.difference(wgslsmith_mutants)
+        covered_by_wgslsmith_only = wgslsmith_mutants.difference(cts_mutants)
+
+        print(f'Covered by both: {len(covered_by_both)}')
+        print(f'Covered by CTS only: {len(covered_by_cts_only)}')
+        print(f'Covered by WGSLsmith only: {len(covered_by_wgslsmith_only)}')
+
+def get_mutants(filepath : Path):
+
+    all_mutants = set()
+
+    for filename in os.listdir(filepath):
+        file = os.path.join(filepath, filename)
+        with open(file) as f:
+            mutants = set(f.readlines())
+
+        all_mutants = all_mutants.union(mutants)
+
+    return all_mutants
+
+if __name__=="__main__":
+    base = Path('/data/dev/dredd-webgpu-testing/llvmpipe')
+    cts_tracking = Path(base,'tracking')
+    wgslsmith_tracking = Path(base, 'wgslsmith', 'tracking')
+    #track(track_cts = False, track_wgslsmith = True)
+    process_tracking(cts_tracking, wgslsmith_tracking)
