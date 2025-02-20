c100bf3eb96f3fe28f3db740668b477f8eb88197
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-02-20 10:56:00 +0000

    Remove isolate flag from cts test script since it causes the run to hang

--- scripts/runners/cts_test_scripts/single_test_lavapipe.sh
+++ scripts/runners/cts_test_scripts/single_test_lavapipe.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-MESA_INSTALL=/data/dev/mesa/build/install
+MESA_INSTALL=/data/dev/mesa/build_sanitized/install
 DAWN=/data/dev/dawn
 
 export VK_ICD_FILENAMES=$MESA_INSTALL/share/vulkan/icd.d/lvp_icd.x86_64.json 
 export LD_PRELOAD=/usr/lib/gcc/x86_64-linux-gnu/12/libasan.so
 
 $DAWN/tools/run run-cts \
-    --isolate \
     --verbose \
     --bin=$DAWN/out/Debug \
     --cts=/data/dev/webgpu_cts \
