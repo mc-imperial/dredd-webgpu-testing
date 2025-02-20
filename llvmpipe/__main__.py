044b21bdc8047b2f5f643f3da72d30e53433d894
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-02-20 10:56:26 +0000

    Add main script to the llvmpipe directory for guiding llvmpipe mutant testing

---
+++ llvmpipe/__main__.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+from pathlib import Path
+
+import run.cts.kill_mutants
+import run.wgslsmith.kill_mutants
+
+def main():
+    
+    # Get list of Mesa mutants to kill
+
+    # Kill mutants with wgslsmith
+
+if __name__=="__main__":
+    main()
