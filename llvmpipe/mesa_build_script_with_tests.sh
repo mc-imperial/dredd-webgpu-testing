407d7e16e676df43e969e394efab8da1328964c9
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-03-21 14:24:26 +0000

    Fix clang error in mesa build scripts

--- llvmpipe/mesa_build_script_with_tests.sh
+++ llvmpipe/mesa_build_script_with_tests.sh
@@ -7,14 +7,11 @@ cd $MESA
 export CC=clang
 export CXX=clang++
 
-#export LD=clang++
-
-export CC_LD=lld
-export CXX_LD=lld
+BUILDDIR=build_tests
 
 source /data/dev/dredd-webgpu-testing/llvmpipe/mesavenv/bin/activate
 
-BUILDDIR=build_sanitized
+meson setup $BUILDDIR
 
 meson setup --reconfigure  \
     --prefix="${MESA}/${BUILDDIR}/install" \
@@ -22,11 +19,7 @@ meson setup --reconfigure  \
     -Dvulkan-drivers=swrast \
     -Dplatforms=x11 \
     -Dincludedir=include \
-    -Db_sanitize=address \
-    -Dc_args=-Wl,-rpath=$(dirname $($CXX --print-file-name libclang_rt.asan-x86_64.so)) \
-    -Dcpp_args=-Wl,-rpath=$(dirname $($CXX --print-file-name libclang_rt.asan-x86_64.so)) \
-    -Db_asneeded=false \
-    -Db_lundef=false \
+    -Dbuild-tests=true \
     $BUILDDIR/
 
 ninja -C $BUILDDIR/
