82a1a5ba078524df054d9416081e7fc3199be5b7
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-03-21 14:30:39 +0000

    Add sanitizer library to lavapipe test

--- scripts/runners/cts_test_scripts/single_test_lavapipe.sh
+++ scripts/runners/cts_test_scripts/single_test_lavapipe.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 MESA_INSTALL=/data/dev/mesa/build_sanitized/install
+#MESA_INSTALL=/data/dev/mesa_tracked/build/install
 DAWN=/data/dev/dawn
-
+#export LD_LIBRARY_PATH=/usr/lib/llvm-18/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so
 export VK_ICD_FILENAMES=$MESA_INSTALL/share/vulkan/icd.d/lvp_icd.x86_64.json 
-export LD_PRELOAD=/usr/lib/gcc/x86_64-linux-gnu/12/libasan.so
+export LD_PRELOAD=/usr/lib/llvm-18/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so
 
 $DAWN/tools/run run-cts \
+    --no-cache \
     --verbose \
     --bin=$DAWN/out/Debug \
     --cts=/data/dev/webgpu_cts \
