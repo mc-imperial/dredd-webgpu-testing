907b2e206fd0cd7a9aacd14ffa5cc9a83ef4715a
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-03-21 14:26:26 +0000

    Reduce crash kills in addition to miscompilation

--- reduce/reduce.py
+++ reduce/reduce.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import argparse
 from pathlib import Path
 import subprocess
 import json
@@ -8,26 +9,43 @@ from run.wgslsmith.utils import get_inputs, get_mutant_killing_tests
 
 def main():
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("kill", choices=['crash','miscompilation'])
+
     base=Path('/data/dev/dredd-webgpu-testing')
     reduce=Path(base,'reduce')
+    out_dir=Path(reduce, 'output', 'crash')
+
     kill_dir=Path(base, 'llvmpipe/output/killed_mutants')
     test_dir=Path(base, 'llvmpipe/output/tests')
-    interesting_test=Path(reduce, 'interestingness.sh')
 
-    mutant_killing_tests = get_mutant_killing_tests(kill_dir)
+    args = parser.parse_args()
 
-    with open(Path(reduce, 'stdout_kills.json'), 'w') as f:
-        json.dump(mutant_killing_tests, f, indent=4)
+
+    if args.kill == 'miscompilation':
+        interesting_test=Path(reduce, 'interestingness.sh')
+        mutant_killing_tests = get_mutant_killing_tests(kill_dir)
+        with open(Path(reduce, 'stdout_kills.json'), 'w') as f:
+            json.dump(mutant_killing_tests, f, indent=4)
+
+    else:
+        interesting_test=Path(reduce, 'interestingness_crash.sh')
+        mutant_killing_tests = {'95847' : 'wgslsmith_3585826504'}
+        with open(Path(reduce, 'crash_kills.json'), 'w') as f:
+            json.dump(mutant_killing_tests, f, indent=4)
 
     for mutant, test in mutant_killing_tests.items():
+
         seed = test[test.find('_')+1:]
 
         wgslsmith_program = Path(test_dir, test, 'original_prog.wgsl')
         input_path = Path(reduce, 'inputs.json')
-        mutant_path = Path(reduce, 'mutant.txt')
-        output_path = Path(reduce, 'output', 'with_creduce_pass', f'mutant_{mutant}')
+        
+        output_path = Path(out_dir, f'mutant_{mutant}')     
         interesting_path = Path(output_path, 'interestingness.sh')
         prog_path = Path(output_path, 'original.wgsl')
+        mutant_path = Path(output_path, 'mutant.txt')
 
         # Create output directory - interestingness test and original program must be in same folder
         try:
@@ -39,10 +57,15 @@ def main():
         shutil.copyfile(interesting_test, interesting_path)
         cmd = ['chmod','+x','interestingness.sh']
         subprocess.run(cmd, cwd=output_path)
-        
-        # Record mutant ID so that it can be accessed by the interestingness script
-        with open(mutant_path, 'w') as f:
-            f.write(mutant)
+
+        # Write mutant ID into interestingness script
+        with open(interesting_path,'r') as f:
+            code = f.readlines()
+
+        code.insert(1, f'MUTANT={mutant}')
+
+        with open(interesting_path,'w') as f:
+            f.writelines(code)
 
         # Perses removes comments from start of file, so must extract inputs before we start reducing
         inputs = get_inputs(wgslsmith_program)
@@ -57,12 +80,10 @@ def main():
             '/data/dev/perses/bazel-bin/src/org/perses/perses_deploy.jar', 
             '--test-script', str(interesting_path),
             '--input-file', str(prog_path),
-            '-o', str(Path(output_path,'reduce'))]
+            '-o', str(Path(output_path,'reduce')),
+            '--call-creduce', 'True']
         
-
         subprocess.run(cmd)
-
-        exit()
 
 if __name__=="__main__":
     main()
