f392e4a6c5f739bc07858a344f929f4cb7458629
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-03-21 16:42:01 +0000

    Add script to kill lavapipe mutants with the cts

---
+++ scripts/lavapipe/kill_with_cts.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+BASE=/data/dev
+
+DAWN=$BASE/dawn/out/Debug/dawn.node
+OUTPUT=$BASE/dredd-webgpu-testing/llvmpipe/output
+CTS=$BASE/webgpu_cts
+
+MESA_MUTATED=$BASE/mesa_mutated
+MESA_TRACKED=$BASE/mesa_tracked
+
+VK_ICD_MESA_MUTATED=$MESA_MUTATED/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
+VK_ICD_MESA_TRACKED=$MESA_TRACKED/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
+
+INFO_MUTATED=$MESA_MUTATED/mutation_info.json
+INFO_TRACKED=$MESA_TRACKED/mutation_info.json
+
+CTS_TRACKING=$BASE/dredd-webgpu-testing/llvmpipe/tracking
+WGSLSMITH_TRACKING=$BASE/dredd-webgpu-testing/llvmpipe/wgslsmith/tracking
+
+TARGET_MUTANT_FILE=$OUTPUT/cts_mutant_targets.txt
+
+cd $BASE/dredd-webgpu-testing
+
+source venv/bin/activate
+
+python llvmpipe \
+    $OUTPUT \
+    $VK_ICD_MESA_MUTATED \
+    $VK_ICD_MESA_TRACKED \
+    $INFO_MUTATED \
+    $INFO_TRACKED \
+    $CTS_TRACKING \
+    $WGSLSMITH_TRACKING \
+    $DAWN \
+    kill_with_cts \
+    $CTS \
+    'cts_intersect_wgslsmith' \
+    $TARGET_MUTANT_FILE \
+    1000 
