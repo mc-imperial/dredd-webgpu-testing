d6c3f0a029a7c2ef3475c320d81e6e5d2bd128b0
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-03-21 14:31:13 +0000

    Mesa demo sanitized check script

---
+++ llvmpipe/sanitized/mesa_demo_sanitized.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+MESA=/data/dev/mesa/build_sanitized
+MESA_DEMO=/data/dev/mesa_demos/build/src/demos
+export VK_ICD_FILENAMES=${MESA}/install/share/vulkan/icd.d/lvp_icd.x86_64.json
+export LD_PRELOAD=/usr/lib/llvm-18/lib/clang/18/lib/linux/libclang_rt.asan-x86_64.so
+
+${MESA_DEMO}/bounce
