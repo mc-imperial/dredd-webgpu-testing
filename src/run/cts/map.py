c105adf9cd61fe0b4ea7ee247b4db5051ae990e8
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-06-17 11:44:46 +0100

    Move tracking code to tracking module

--- src/run/cts/map.py
+++ src/run/cts/map.py
@@ -6,7 +6,6 @@ import sys
 import json
 import pandas as pd
 
-from mutate.mutate_mesa import clean, get_files_for_mutation, mutate
 from run.cts.utils import run_cts
 from run.wgslsmith.utils import run_wgslsmith_program
 
@@ -132,94 +131,3 @@ def get_mutants_to_kill(cts_tracking, wgslsmith_tracking, mutant_file, n_sample=
 
     return mutant_sample
 
-    
-def track_mesa(mesa, dredd, mutation_dir, info_file, compile_commands):
-
-    clean(mesa)
-
-    mutation_files = get_files_for_mutation(compile_commands, 
-                                        mutation_dir, 
-                                        mesa)
-
-    mutate(dredd,
-        mutation_files,
-        info_file,
-        compile_commands,
-        mesa,
-        track_only=True)
-
-    build(mesa)
-    install(mesa)
-
-def track(track_cts : bool = True, track_wgslsmith : bool = True, n : int = 1):
-
-    base = Path('/data/dev')
-    dredd = Path(base,'dredd/third_party/clang+llvm/bin/dredd')
-    mesa = Path(base, 'mesa_tracked')
-    mutation_dir = Path('src')
-    info_file = Path(mesa,'mutant_info.json')
-    compile_commands = Path(mesa, 'build', 'compile_commands.json')
-
-    cts = Path('/data/dev/webgpu_cts')
-    dawn = Path('/data/dev/dawn')
-    mesa_vk_icd = Path(mesa, 'build/install/share/vulkan/icd.d/lvp_icd.x86_64.json')
-
-    if track_cts:
-        for i in range(n):
-            tracking_file = Path(f'tracking/tracking_file_run_{i}.txt').resolve()
-            run_cts(cts, dawn, mesa_vk_icd, output_name = f'tracking/tracking_run_{i}', tracking_file = tracking_file)
-
-    if track_wgslsmith:
-        for i in range(100):
-            tracking_file = Path(f'wgslsmith/tracking/tracking_file_wgslsmith_run_{i}.txt').resolve()
-            wgslsmith_program = Path(f'wgslsmith/wgslsmith_prog_{i}.js').resolve()
-            run_wgslsmith_program(wgslsmith_program,
-                mesa_vk_icd,
-                generate=True,
-                tracking=tracking_file)
-
-def process_tracking(cts : Path, wgslsmith : Path) -> dict[str,list[int]]:
-
-    if cts is not None:
-        cts_mutants = get_mutants(cts)
-        print(f'The CTS covers {len(cts_mutants)} mutants in total')
-
-    if wgslsmith is not None:
-        wgslsmith_mutants = get_mutants(wgslsmith)
-        print(f'A sample of 100 WGSLsmith tests cover {len(wgslsmith_mutants)} mutants in total')
-
-    if cts is not None and wgslsmith is not None:
-        covered_by_both = cts_mutants.intersection(wgslsmith_mutants)
-        covered_by_cts_only = cts_mutants.difference(wgslsmith_mutants)
-        covered_by_wgslsmith_only = wgslsmith_mutants.difference(cts_mutants)
-
-        print(f'Covered by both: {len(covered_by_both)}')
-        print(f'Covered by CTS only: {len(covered_by_cts_only)}')
-        print(f'Covered by WGSLsmith only: {len(covered_by_wgslsmith_only)}')
-
-    coverage = {'covered_by_both' : covered_by_both,
-                'covered_by_cts_only' : covered_by_cts_only,
-                'covered_by_wgslsmith_only' : covered_by_wgslsmith_only
-                }
-
-    return coverage
-
-def get_mutants(filepath : Path):
-
-    all_mutants = set()
-
-    for filename in os.listdir(filepath):
-        file = os.path.join(filepath, filename)
-        with open(file) as f:
-            mutants = set(f.readlines())
-
-        all_mutants = all_mutants.union(mutants)
-
-    return all_mutants
-
-if __name__=="__main__":
-    base = Path('/data/dev/dredd-webgpu-testing/llvmpipe')
-    tracking_dir = Path(base, 'output', 'covered_by_cts', 'test_wise_tracking', 'tracking_files')
-    output_dir = Path(base, 'output', 'covered_by_cts', 'test_wise_tracking')
-    mapping = Path(base, 'output', 'covered_by_cts', 'test_wise_tracking', 'mapping_test_to_id.json' )
-    process_test_wise_tracking(tracking_dir, output_dir, mapping)
--- src/track/__main__.py
+++ src/track/__main__.py
@@ -39,3 +39,70 @@ def main():
         args.output,
         args.vk_icd,
         args.query)
+
+
+def track(track_cts : bool = True, track_wgslsmith : bool = True, n : int = 1):
+
+    base = Path('/data/dev')
+    dredd = Path(base,'dredd/third_party/clang+llvm/bin/dredd')
+    mesa = Path(base, 'mesa_tracked')
+    mutation_dir = Path('src')
+    info_file = Path(mesa,'mutant_info.json')
+    compile_commands = Path(mesa, 'build', 'compile_commands.json')
+
+    cts = Path('/data/dev/webgpu_cts')
+    dawn = Path('/data/dev/dawn')
+    mesa_vk_icd = Path(mesa, 'build/install/share/vulkan/icd.d/lvp_icd.x86_64.json')
+
+    if track_cts:
+        for i in range(n):
+            tracking_file = Path(f'tracking/tracking_file_run_{i}.txt').resolve()
+            run_cts(cts, dawn, mesa_vk_icd, output_name = f'tracking/tracking_run_{i}', tracking_file = tracking_file)
+
+    if track_wgslsmith:
+        for i in range(100):
+            tracking_file = Path(f'wgslsmith/tracking/tracking_file_wgslsmith_run_{i}.txt').resolve()
+            wgslsmith_program = Path(f'wgslsmith/wgslsmith_prog_{i}.js').resolve()
+            run_wgslsmith_program(wgslsmith_program,
+                mesa_vk_icd,
+                generate=True,
+                tracking=tracking_file)
+
+def process_tracking(cts : Path, wgslsmith : Path) -> dict[str,list[int]]:
+
+    if cts is not None:
+        cts_mutants = get_mutants(cts)
+        print(f'The CTS covers {len(cts_mutants)} mutants in total')
+
+    if wgslsmith is not None:
+        wgslsmith_mutants = get_mutants(wgslsmith)
+        print(f'A sample of 100 WGSLsmith tests cover {len(wgslsmith_mutants)} mutants in total')
+
+    if cts is not None and wgslsmith is not None:
+        covered_by_both = cts_mutants.intersection(wgslsmith_mutants)
+        covered_by_cts_only = cts_mutants.difference(wgslsmith_mutants)
+        covered_by_wgslsmith_only = wgslsmith_mutants.difference(cts_mutants)
+
+        print(f'Covered by both: {len(covered_by_both)}')
+        print(f'Covered by CTS only: {len(covered_by_cts_only)}')
+        print(f'Covered by WGSLsmith only: {len(covered_by_wgslsmith_only)}')
+
+    coverage = {'covered_by_both' : covered_by_both,
+                'covered_by_cts_only' : covered_by_cts_only,
+                'covered_by_wgslsmith_only' : covered_by_wgslsmith_only
+                }
+
+    return coverage
+
+def get_mutants(filepath : Path):
+
+    all_mutants = set()
+
+    for filename in os.listdir(filepath):
+        file = os.path.join(filepath, filename)
+        with open(file) as f:
+            mutants = set(f.readlines())
+
+        all_mutants = all_mutants.union(mutants)
+
+    return all_mutants
