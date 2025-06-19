1b3d759eb7e8d545c5fe2614895588b88d139444
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-06-19 17:58:25 +0100

    List mutants and queries to kill with CTS

--- src/kill/__main__.py
+++ src/kill/__main__.py
@@ -1,4 +1,9 @@
 import argparse
+import pandas as pd
+
+from run.cts.map import get_least_covered_mutants
+import run.cts.kill_mutants
+import run.wgslsmith.kill_mutants
 
 def main():
     
@@ -27,9 +32,60 @@ def main():
             type=Path,
             help='Path to list of mutants touched by a sample of WGSLsmith tests',
             default='')
+    parser.add_argument('--mutant_ids',
+            type=comma_list,
+            help='List of specific mutant IDs to kill'
+            default=None)
+    parser.add_argument('--sample',
+            type=int,
+            help='Number of mutants to kill'
+            default=3)
 
     args = parser.parse_args()
 
+    # Get list of mutants to target
+    mutants_to_kill = []
+
+    if args.mutant_ids is not None:
+        mutants_to_kill = args.mutant_ids
+
+    mutants_to_kill = get_least_covered_mutants(args.map,
+        args.wgslsmith_touched,
+        args.sample)
+
+    mutant_to_test_mapping = pd.read_csv(args.map)
+
+    if len(mutants_to_kill) == 0:
+        print('No mutants to kill!')
+        exit(1)
+
+    # Kill mutants with CTS
+    print(f'Number of mutants to kill: {len(mutants_to_kill)}')
+
+    for mutant in mutants_to_kill:
+
+        queries = mutant_to_test_mapping.loc[mutant_to_test_mapping['mutant_id'] == int(mutant), 'queries'].iloc[0]
+        queries = queries.split(' ')
+
+        print(f'Attempting mutant {mutant}, which is touched by {len(queries)} tests...')
+
+        '''
+        cts_args=[str(args.info_file_mutated),
+            str(args.info_file_tracked), 
+            str(args.output), # mutant_kill_path
+            '--mutant_sample', f'''{','.join([str(m) for m in mutants_to_kill])}''',
+            '--mutant_to_test_mapping', str(args.mutant_to_test_mapping),
+            'mesa',
+            str(args.dawn),
+            str(args.mutated_vk_icd), 
+            str(args.tracked_vk_icd)
+            ]
+
+        run.cts.kill_mutants.main(cts_args)
+        '''
+
+def comma_list(arg):
+    return arg.split(',')
 
 if __name__=="__main__":
     main()
