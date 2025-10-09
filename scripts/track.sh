e2c2700b2299eaadd4304f5f0334fcff347bc5e2
ambergorzynski <amber.gorzynski@gmail.com>
2025-10-09 14:16:02 +0000

    Capture output for mutant tracking

--- scripts/track.sh
+++ scripts/track.sh
@@ -5,8 +5,15 @@ DAWN=$BASE/dawn
 CTS=$BASE/webgpu_cts
 VK_ICD=$BASE/mesa_tracked/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
 
+TIMESTAMP="$(date +"%s")"
+
+OUTPUT=$BASE/dredd-webgpu-testing/data/cts_output_$TIMESTAMP.txt
+
+rm -rf $BASE/dredd-webgpu-testing/data/tracking_files
+
 python -m track cts \
 	$VK_ICD \
 	$DAWN \
 	--cts $CTS \
-	--query 'webgpu:shader,execution,shadow:*'
+	--query 'webgpu:shader,execution,*' \
+	| tee $OUTPUT
