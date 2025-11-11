5ffe915db7db53027c5ba145821f11ea55bfa37d
ambergorzynski <amber.gorzynski@gmail.com>
2025-11-11 09:16:30 +0000

    Add reset option to utils

--- src/common/utils.py
+++ src/common/utils.py
@@ -27,12 +27,14 @@ class FileInfo:
         src : Path, 
         compile_commands : Path, 
         mutant_info_file : Path,
-        track_only : bool):
+        track_only : bool,
+        reset: bool):
 
         self.src : Path = src
         self.compile_commands : Path = compile_commands
         self.mutant_info_file : Path = mutant_info_file
         self.track_only : bool = track_only
+        self.reset : bool = reset
         self.mutation_files : list[str] = []
 
     def __str__(self):
