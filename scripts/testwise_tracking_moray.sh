98f4d6865747827f56c622bd8327142c6e6e8939
agg22 <amber.gorzynski22@imperial.ac.uk>
2025-11-25 14:57:35 +0000

    Add tracking script for moray

---
+++ scripts/testwise_tracking_moray.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MESA_SHADER_CACHE_DISABLE=true
+
+QUERY='webgpu:*'
+BASE='/data/dev'
+
+cd '/data/dev/dredd-webgpu-testing/src'
+
+python analyse/check_reset_entrypoint.py \
+	--query "$QUERY" \
+	--mutate \
+    --base $BASE \
+	--run_joint 
+
+
