1ced417f0e19f164f564ca6e36d723024b3f7e70
agg22 <amber.gorzynski22@imperial.ac.uk>
2026-02-16 22:00:13 +0000

    Update tracking script

--- scripts/track.sh
+++ scripts/track.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-BASE=/home/ubuntu/dev
+BASE=/data/dev
 DAWN=$BASE/dawn
 CTS=$BASE/webgpu_cts
 VK_ICD=$BASE/mesa_tracked/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
-
-TIMESTAMP="$(date +"%s")"
-
-OUTPUT=$BASE/dredd-webgpu-testing/data/cts_output_$TIMESTAMP.txt
-
-rm -rf $BASE/dredd-webgpu-testing/data/tracking_files
+OUTPUT=$BASE/dredd-webgpu-testing/data
 
 python -m track cts \
-	$VK_ICD \
-	$DAWN \
-	--cts $CTS \
-	--query 'webgpu:shader,execution,flow_control,*'
-#| tee $OUTPUT
+    --vk-icd $VK_ICD \
+    --dawn $DAWN \
+    --cts $CTS \
+    --output $OUTPUT \
+    --query 'webgpu:*'
+
+
+#    --query 'webgpu:shader,execution,expression,access,array,index:concrete_scalar:*'
