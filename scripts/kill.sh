8926267fccc0d762a1ee04699f335253c7b1fa23
agg22 <amber.gorzynski22@imperial.ac.uk>
2026-02-24 14:23:47 +0000

    Update kill script to match new kill structure

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -2,19 +2,15 @@
 
 BASE=/data/dev
 INFOMUTATED=$BASE/mesa_mutated/mutation_info.json
-INFOTRACKED=$BASE/mesa_tracked/mutation_info.json
 VKICDMUTATED=$BASE/mesa_mutated/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
-VKICDTRACKED=$BASE/mesa_tracked/build/install/share/vulkan/icd.d/lvp_icd.x86_64.json
 DAWN=$BASE/dawn
 CTS=$BASE/webgpu_cts
-MAP=$BASE/data/mutants_to_kill.csv
+MAP=$BASE/dredd-webgpu-testing/data/mutants_to_kill.csv
 
 python -m kill \
     $INFOMUTATED \
-    $INFOTRACKED \
     $VKICDMUTATED \
-    $VKICDTRACKED \
     $DAWN \
     --cts $CTS \
     --map $MAP \
-    --sample 5
+    --sample 2
