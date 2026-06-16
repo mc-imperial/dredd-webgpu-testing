0e3a999262b3093cc33350435b3a9336232595b3
agg22 <amber.gorzynski22@imperial.ac.uk>
2026-06-16 12:00:16 +0100

    Add .env file with commit hashes

---
+++ issta_2026/build/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+source ./.env
+
+echo $DREDD_COMMIT
+
+# Build Dredd
+
+# Build Mesa (twice - once for mutant tracking and once for mutant injection)
+
+# Build Dawn
+
+# Build WGSLsmith
